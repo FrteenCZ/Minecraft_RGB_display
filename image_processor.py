0b6ffd8d18d491ea80a29ba78dcfca6cf8010db4
FrteenCZ <franta11franta@gmail.com>
2025-08-23 18:53:29 +0200

    Refactor image processing logic to the actual display system in game

--- image_processor.py
+++ image_processor.py
@@ -2,46 +2,24 @@ import cv2
 import matplotlib.pyplot as plt
 import numpy as np
 
-# Load an image (color)
-image = cv2.imread("miku.png", cv2.IMREAD_COLOR)
+input = cv2.imread("miku.png", cv2.IMREAD_COLOR)
+input = cv2.resize(input, (100, 100))
 
-image = cv2.resize(image, (100, 100))
+output = np.zeros_like(input, dtype=np.uint8)
 
-img = np.zeros((image.shape[0] + 2, image.shape[1], image.shape[2]), dtype=np.float32)
-img[2:, :, :] = image
+for y in range(-1, output.shape[0] + 1):
+    for x in range(-1, output.shape[1]):
+        if (y + x) % 2 == 0:
 
-for i in range(2):
-    img[i, :, :] = img[2, :, :]
+            y0 = np.clip(y - 1, 0, output.shape[0])
+            y1 = np.clip(y + 2, 0, output.shape[0])
+            x0 = np.clip(x, 0, output.shape[1])
+            x1 = np.clip(x + 2, 0, output.shape[1])
 
-
-
-kernel_x_size = 2
-kernel_y_size = 3
-
-# Create an output image
-print(f"Image shape: {img.shape}")
-output = np.zeros_like(img, dtype=np.float32)
-print(f"Image shape: {output.shape}")
-
-# Loop over blocks without overlap
-for channel in range(output.shape[2]):  
-    for y in range(0, output.shape[0], kernel_y_size):
-        y += channel
-        for x in range(0, output.shape[1], kernel_x_size):
-            block = img[y:y+kernel_y_size, x:x+kernel_x_size]
-        
-            # Compute mean of the block
-            mean_val = np.mean(block[:, :, channel], dtype=np.float32)
-            
-            # Fill the block in the output with that mean
-            output[y:y+kernel_y_size, x:x+kernel_x_size, channel] = mean_val
-
-output = output[2:, :, :]
+            c = (y + 1) % 3
+            output[y0:y1, x0:x1, c] = np.average(input[y0:y1, x0:x1, c])
 
 cv2.imwrite("output.png", output)
 
-plt.imshow(cv2.cvtColor(output, cv2.COLOR_BGR2RGB)// 16 * 16 / 255.0)
+plt.imshow(cv2.cvtColor(output, cv2.COLOR_BGR2RGB))
 plt.show()
-
-if __name__ == "__main__":
-    pass
